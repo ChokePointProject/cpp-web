c19c3b14f91485b1956876c4e1521be07ad94616
Javier Arturo Rodriguez <codehead@gmail.com>
2011-11-17 07:18:00 +0100

    Fixed issues #5, #6, #9

--- root/static/js/script.js
+++ root/static/js/script.js
@@ -1,8 +1,18 @@
 /* Author:
 
-*/
+ */
 
-
-
-
-
+$(document).ready(function() {
+	$('a.panel').click(function() {
+		var panel = $(this);
+		var id = panel.attr('id');
+		if (panel.hasClass('open')) {
+			panel.removeClass('open').addClass('closed');
+			$('#panel_' + id).hide();
+		} else {
+			panel.removeClass('closed').addClass('open');
+			$('#panel_' + id).show();
+		}
+		return false;
+	});
+});
